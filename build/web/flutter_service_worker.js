2c423da05c0e9dc4cf0804e137ea2d25455165fd
dhairya <joshidhairaya2002@gmail.com>
2023-04-19 23:24:18 +0530

    logout added

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "59f95281e6b5b522b399c865ab34a84a",
-"/": "59f95281e6b5b522b399c865ab34a84a",
-"main.dart.js": "b21cd6a1ac5dc8b87022b55e8ade459e",
+"index.html": "ee1f25e9d171d4dda34c8559d252a120",
+"/": "ee1f25e9d171d4dda34c8559d252a120",
+"main.dart.js": "04e1c5fd553ce01c91ba071422f79e6b",
 "manifest.json": "f0e610b39be8dfbf1513d40b33d5e320",
 "version.json": "65c333cc28b44316be01e4c2eb7bcdb2"
 };
