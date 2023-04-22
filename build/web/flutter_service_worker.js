0fa01289a184f0993e0ae053f8c2774227d9cd97
dhairya <joshidhairaya2002@gmail.com>
2023-04-22 22:04:53 +0530

    update

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "112cb077203d32bbbf16aaacb90a6a6e",
-"/": "112cb077203d32bbbf16aaacb90a6a6e",
-"main.dart.js": "d5dfdf1991f5374b06b6c7364e92a052",
+"index.html": "95a1997adc862c599deb1bfb3e458d6f",
+"/": "95a1997adc862c599deb1bfb3e458d6f",
+"main.dart.js": "0509f16ea4221a04a8207a9e8292d732",
 "manifest.json": "f0e610b39be8dfbf1513d40b33d5e320",
 "version.json": "65c333cc28b44316be01e4c2eb7bcdb2"
 };
