d5a1bd67fa5fdd521c67acdddffdcde21720d7e0
dhairya <joshidhairaya2002@gmail.com>
2023-05-03 22:43:08 +0530

    made bloc uniform across all screens

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "65da784c5012c03b9a0f9a14316e3a84",
-"/": "65da784c5012c03b9a0f9a14316e3a84",
-"main.dart.js": "3b1da4ef83225e2e287e4f5bbbbac82a",
+"index.html": "b3044dacd27540765db0c563c30af2b4",
+"/": "b3044dacd27540765db0c563c30af2b4",
+"main.dart.js": "8d7d885f926dd162af8b5793c54d8943",
 "manifest.json": "9a1481382c7ab7111b7d2946a0d753b6",
 "version.json": "65c333cc28b44316be01e4c2eb7bcdb2"
 };
--- lib/screens/desktopchat.dart
+++ lib/screens/desktopchat.dart
@@ -18,185 +18,184 @@ ChatBloc _chatBloc = ChatBloc();
 class DesktopChatScreen extends StatelessWidget {
   String response = "", search = "";
   bool sh = false;
-  TextEditingController _controller = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
   double temp = 0;
+  BuildContext ctx;
+  DesktopChatScreen(this.ctx, {super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocProvider(
-        create: ((context) => _chatBloc),
-        child: Container(
-          color: Color.fromRGBO(52, 53, 65, 1),
-          width: double.infinity,
-          height: double.infinity,
-          child: Row(
-            children: [
-              Flexible(
-                  flex: MediaQuery.of(context).size.width > 1230 ? 3 : 4,
-                  fit: FlexFit.tight,
-                  child: BlocBuilder<ChatBloc, AppState>(
-                    builder: (context, state) {
-                      if (state is ChatLoadingState) {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      } else if (state is ChatLoadedState) {
-                        return ChatSidebar(context, state);
-                      } else if (state is ImageGenerationState) {
-                        return ImageSideBar(context, state);
-                      } else if (state is UnderProgressState) {
-                        return UPChatSidebar(context, state);
-                      } else {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    },
-                  )),
-              Flexible(
-                  flex: 11,
-                  child: Container(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 15,
-                    ),
-                    child: Column(
-                      children: [
-                        Flexible(
-                            flex: 5,
-                            fit: FlexFit.tight,
-                            child: BlocBuilder<ChatBloc, AppState>(
-                              builder: (context, state) {
-                                if (state is ChatLoadingState) {
-                                  return Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                }
-                                if (state is UnderProgressState) {
-                                  return Center(
-                                    child: Text('This is under progress'),
-                                  );
-                                }
-                                if (state is ImageGenerationState) {
-                                  return ImageContent(context, state);
-                                }
-                                if (state is ChatLoadedState) {
-                                  return ChatContent(context, state);
-                                }
+      body: Container(
+        color: Color.fromRGBO(52, 53, 65, 1),
+        width: double.infinity,
+        height: double.infinity,
+        child: Row(
+          children: [
+            Flexible(
+                flex: MediaQuery.of(context).size.width > 1230 ? 3 : 4,
+                fit: FlexFit.tight,
+                child: BlocBuilder<ChatBloc, AppState>(
+                  builder: (ctx, state) {
+                    if (state is ChatLoadingState) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (state is ChatLoadedState) {
+                      return ChatSidebar(ctx, state);
+                    } else if (state is ImageGenerationState) {
+                      return ImageSideBar(ctx, state);
+                    } else if (state is UnderProgressState) {
+                      return UPChatSidebar(ctx, state);
+                    } else {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                  },
+                )),
+            Flexible(
+                flex: 11,
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 15,
+                  ),
+                  child: Column(
+                    children: [
+                      Flexible(
+                          flex: 5,
+                          fit: FlexFit.tight,
+                          child: BlocBuilder<ChatBloc, AppState>(
+                            builder: (ctx, state) {
+                              if (state is ChatLoadingState) {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              }
+                              if (state is UnderProgressState) {
+                                return Center(
+                                  child: Text('This is under progress'),
+                                );
+                              }
+                              if (state is ImageGenerationState) {
+                                return ImageContent(ctx, state);
+                              }
+                              if (state is ChatLoadedState) {
+                                return ChatContent(ctx, state);
+                              }
+                              return SizedBox.shrink();
+                            },
+                          )),
+                      Flexible(
+                          flex: 1,
+                          fit: FlexFit.tight,
+                          child: BlocBuilder<ChatBloc, AppState>(
+                            builder: (ctx, state) {
+                              if (state is ChatLoadedState &&
+                                  ((state.sc &&
+                                          state.selectedDropdown != null) ||
+                                      !state.sc)) {
+                                return Container(
+                                  // color: Colors.white,
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 15),
+                                  child: Row(children: [
+                                    Flexible(
+                                      flex: 6,
+                                      child: TextField(
+                                        controller: _controller,
+                                        keyboardType: TextInputType.multiline,
+                                        onChanged: ((value) {
+                                          search = value;
+                                          BlocProvider.of<ChatBloc>(ctx)
+                                              .add(SetTokenEvent(
+                                                  value.length));
+                                        }),
+                                        onSubmitted: (val) {
+                                          _controller.text = "";
+                                          BlocProvider.of<ChatBloc>(ctx)
+                                              .add(FetchResultEvent(search));
+                                          search = "";
+                                        },
+                                      ),
+                                    ),
+                                    Flexible(
+                                      flex: 1,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          IconButton(
+                                            icon: Icon(Icons.send),
+                                            onPressed: () {
+                                              _controller.text = "";
+                                              BlocProvider.of<ChatBloc>(
+                                                      ctx)
+                                                  .add(FetchResultEvent(
+                                                      search));
+                                              search = "";
+                                            },
+                                          ),
+                                          Text(
+                                            state.tc.toString(),
+                                            style: TextStyle(
+                                                fontSize: 11,
+                                                color: state.tc +
+                                                            state.maxlength >
+                                                        4000
+                                                    ? Colors.red
+                                                    : Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ]),
+                                );
+                              }
+                              if (state is ImageGenerationState) {
+                                return Container(
+                                  // color: Colors.white,
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 15),
+                                  child: Row(children: [
+                                    Flexible(
+                                      flex: 6,
+                                      child: TextField(
+                                        controller: _controller,
+                                        keyboardType: TextInputType.multiline,
+                                        onChanged: ((value) =>
+                                            search = value),
+                                        onSubmitted: (val) {
+                                          _controller.text = "";
+                                          BlocProvider.of<ChatBloc>(ctx)
+                                              .add(FetchResultEvent(search));
+                                          search = "";
+                                        },
+                                      ),
+                                    ),
+                                    Flexible(
+                                      flex: 1,
+                                      child: IconButton(
+                                        icon: Icon(Icons.send),
+                                        onPressed: () {
+                                          _controller.text = "";
+                                          BlocProvider.of<ChatBloc>(ctx)
+                                              .add(FetchResultEvent(search));
+                                          search = "";
+                                        },
+                                      ),
+                                    ),
+                                  ]),
+                                );
+                              } else {
                                 return SizedBox.shrink();
-                              },
-                            )),
-                        Flexible(
-                            flex: 1,
-                            fit: FlexFit.tight,
-                            child: BlocBuilder<ChatBloc, AppState>(
-                              builder: (context, state) {
-                                if (state is ChatLoadedState &&
-                                    ((state.sc &&
-                                            state.selectedDropdown != null) ||
-                                        !state.sc)) {
-                                  return Container(
-                                    // color: Colors.white,
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 15),
-                                    child: Row(children: [
-                                      Flexible(
-                                        flex: 6,
-                                        child: TextField(
-                                          controller: _controller,
-                                          keyboardType: TextInputType.multiline,
-                                          onChanged: ((value) {
-                                            search = value;
-                                            BlocProvider.of<ChatBloc>(context)
-                                                .add(SetTokenEvent(
-                                                    value.length));
-                                          }),
-                                          onSubmitted: (val) {
-                                            _controller.text = "";
-                                            BlocProvider.of<ChatBloc>(context)
-                                                .add(FetchResultEvent(search));
-                                            search = "";
-                                          },
-                                        ),
-                                      ),
-                                      Flexible(
-                                        flex: 1,
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            IconButton(
-                                              icon: Icon(Icons.send),
-                                              onPressed: () {
-                                                _controller.text = "";
-                                                BlocProvider.of<ChatBloc>(
-                                                        context)
-                                                    .add(FetchResultEvent(
-                                                        search));
-                                                search = "";
-                                              },
-                                            ),
-                                            Text(
-                                              state.tc.toString(),
-                                              style: TextStyle(
-                                                  fontSize: 11,
-                                                  color: state.tc +
-                                                              state.maxlength >
-                                                          4000
-                                                      ? Colors.red
-                                                      : Colors.white),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ]),
-                                  );
-                                }
-                                if (state is ImageGenerationState) {
-                                  return Container(
-                                    // color: Colors.white,
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 15),
-                                    child: Row(children: [
-                                      Flexible(
-                                        flex: 6,
-                                        child: TextField(
-                                          controller: _controller,
-                                          keyboardType: TextInputType.multiline,
-                                          onChanged: ((value) =>
-                                              search = value),
-                                          onSubmitted: (val) {
-                                            _controller.text = "";
-                                            BlocProvider.of<ChatBloc>(context)
-                                                .add(FetchResultEvent(search));
-                                            search = "";
-                                          },
-                                        ),
-                                      ),
-                                      Flexible(
-                                        flex: 1,
-                                        child: IconButton(
-                                          icon: Icon(Icons.send),
-                                          onPressed: () {
-                                            _controller.text = "";
-                                            BlocProvider.of<ChatBloc>(context)
-                                                .add(FetchResultEvent(search));
-                                            search = "";
-                                          },
-                                        ),
-                                      ),
-                                    ]),
-                                  );
-                                } else {
-                                  return SizedBox.shrink();
-                                }
-                              },
-                            ))
-                      ],
-                    ),
-                    // color: Colors.blue,
-                  )),
-            ],
-          ),
+                              }
+                            },
+                          ))
+                    ],
+                  ),
+                  // color: Colors.blue,
+                )),
+          ],
         ),
       ),
     );
